7cec8b608ca548f73a93e2819a41a648533c273c
Tianyu Zhu <41713096+zihan987@users.noreply.github.com>
2023-08-24 01:25:06 +0800

    Update config.lua

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -116,4 +116,15 @@ lvim.plugins = {
       require("spectre").setup()
     end,
   },
+  -- numb.nvim
+  {
+  "nacro90/numb.nvim",
+  event = "BufRead",
+  config = function()
+  require("numb").setup {
+    show_numbers = true, -- Enable 'number' for the window while peeking
+    show_cursorline = true, -- Enable 'cursorline' for the window while peeking
+  }
+  end,
+},
 }
